a4e2206ef3d86977581ae867f4fb64762641a4c3
Jeff Louie <jpl7690@yahoo.com>
2014-12-30 16:31:49 -0800

    changed yummly service to https to work on heroku

--- public/js/services/yummly-service.js
+++ public/js/services/yummly-service.js
@@ -1,5 +1,5 @@
 app.factory('YummlyRestangular', function(Restangular){
-  var basePath = 'http://api.yummly.com/v1';
+  var basePath = 'https://api.yummly.com/v1';
 
   return Restangular.withConfig(function(RestangularConfigurer){
     RestangularConfigurer.setBaseUrl(basePath);
